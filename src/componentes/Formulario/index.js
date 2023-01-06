2b46bae5d063ab1e281257c4665cd85632b94dfa
Victor <victor.eugennio@gmail.com>
2023-01-06 13:45:38 -0300

    Criando o componente de lista e o componente do botão.
    TODO: Prevenir o comportamento padrão do Botão e fazer com que ele não recarregue a página

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,19 @@
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './formulario.css'
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data-Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
     return (
         <section className="formulario">
             <form>
@@ -9,6 +21,10 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite o seu Nome" />
                 <CampoTexto label="Cargo" placeholder="Digite o seu Cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da Imagem" />
+                <ListaSuspensa label="Time" itens={times}/>
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
